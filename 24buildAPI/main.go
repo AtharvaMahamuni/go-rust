c7a223e1a5429a89dc10f4777f17f1fddba58ed1
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2024-02-28 20:22:53 +0530

    Add course

--- 24buildAPI/main.go
+++ 24buildAPI/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -60,5 +63,29 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode("No course found with given id")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if: body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	// generate unique id, string
+	// append course into courses
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	json.NewEncoder(w).Encode(course)
 	return
 }
