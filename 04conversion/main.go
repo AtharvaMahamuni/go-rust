0bab3fb25ed6d72f3ccabda606444d1e7ed2b684
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2024-04-08 12:15:24 +0530

    practice

--- 04conversion/main.go
+++ 04conversion/main.go
@@ -1,36 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome, to type conversion")
+	fmt.Println("Welcome to conversion in golang!")
 
 	// Reader
-	reader := bufio.NewReader(os.Stdin);
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Please enter rating for our Pizza: ");
-	input, _ := reader.ReadString('\n');
+	fmt.Print("Please give rating for our pizza out of 10: ")
+	input, _ := reader.ReadString('\n')
 
-	// removing '/n' from input
-	rating := strings.TrimSpace(input);
+	// to remove '/n'
+	rating := strings.TrimSpace(input)
 
-	fmt.Printf("Thanks for rating, %s! \n", rating);
+	fmt.Printf("Type of rating is %T\n", rating)
 
-	fmt.Printf("Type of rating is : %T \n", rating);
+	floatRating, err := strconv.ParseFloat(rating, 64)
+	intRating := int(math.Round(floatRating))
 
-	// type conversion 
-	floatRating, err := strconv.ParseFloat(rating, 64);
-
-	if err != nil {
-		fmt.Println(err);
+	if err == nil {
+		if intRating > 0 && intRating < 11 {
+			fmt.Println("Thanks for rating, ", intRating)
+			fmt.Println("Rating + 1 is ", intRating+1)
+		} else {
+			fmt.Println("Enter between 0 and 10")
+		}
 	} else {
-		fmt.Println("Rating + 1 is : ", floatRating+1);
+		panic(err)
 	}
 
 }
