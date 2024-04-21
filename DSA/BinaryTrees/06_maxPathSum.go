4c079d6e3b74f0f44adbb4bfed46455feccbb839
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2024-04-21 16:21:03 +0530

    removed wrong solution

--- DSA/BinaryTrees/06_maxPathSum.go
+++ DSA/BinaryTrees/06_maxPathSum.go
@@ -1,32 +1,5 @@
 package main
 
-func maxPathSumDFS(root *NodeInt) int {
-	if root == nil {
-		return 0
-	}
-
-	stack := make([]*NodeInt, 1)
-	stack[0] = root
-	maxSum := 0
-
-	for len(stack) > 0 {
-		current := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-
-		if current.Left != nil && current.Right != nil {
-			if current.Left.Val > current.Right.Val {
-				maxSum += current.Left.Val
-			}
-
-			if current.Right.Val > current.Left.Val {
-				maxSum += current.Right.Val
-			}
-		}
-	}
-
-	return maxSum
-}
-
 func helperSum(root *NodeInt, sum int) int {
 	if root == nil {
 		return 0
