501cf6829493e9d4a4e5f257f5937190f4af2137
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2024-05-13 16:07:08 +0530

    Collection and my first go server

--- 25mongoapi/router/routers.go
+++ 25mongoapi/router/routers.go
@@ -10,7 +10,7 @@ func Router() *mux.Router {
 
 	router.HandleFunc("/api/movies", controller.GetAll).Methods("GET")
 	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
-	router.HandleFunc("/api/movie/{id}", controller.GetAll).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatched).Methods("PUT")
 	router.HandleFunc("/api/movie/{id}", controller.DeleteOne).Methods("DELETE")
 	router.HandleFunc("/api/movies", controller.DeleteAll).Methods("DELETE")
 
