e7024ae2453da63adc1afbfc8b93f56cbe6da39a
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2024-08-01 00:08:49 +0530

    user login and signup, jwt

--- Projects/restaurant-management/controllers/userController.go
+++ Projects/restaurant-management/controllers/userController.go
@@ -2,17 +2,20 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/atharvamahamuni/golang/projects/restaurant-management/database"
+	helper "github.com/atharvamahamuni/golang/projects/restaurant-management/helpers"
 	"github.com/atharvamahamuni/golang/projects/restaurant-management/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.DBinstance("user")
@@ -121,23 +124,77 @@ func SignUp() gin.HandlerFunc {
 			return
 		}
 
-		user.Created_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-		user.Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.ID = primitive.NewObjectID()
 		user.User_id = user.ID.Hex()
+
+		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, user.User_id)
+		user.Token = &token
+		user.Refresh_token = &refreshToken
+
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user was not created"})
+			return
+		}
+
+		c.JSON(http.StatusOK, resultInsertionNumber)
 	}
 }
 
 func Login() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user not found, login seems to be incorrect"})
+			return
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		if passwordIsValid != true {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user password is incorrect"})
+			return
+		}
+
+		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.First_name, *&foundUser.Last_name, *&foundUser.User_id)
+
+		helper.UpdateAllTokens(token, refreshToken, foundUser.User_id)
+
+		c.JSON(http.StatusOK, foundUser)
 	}
 }
 
 func HashPassword(password string) string {
-	return ""
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return string(bytes)
 }
 
-func VerifyPassword(userPassword string, providePassword string) (bool, string) {
-	return false, ""
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("Login of password is incorrect")
+		check = false
+	}
+
+	return check, msg
 }
--- Projects/restaurant-management/go.mod
+++ Projects/restaurant-management/go.mod
@@ -7,6 +7,8 @@ require (
 	go.mongodb.org/mongo-driver v1.16.0
 )
 
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
@@ -35,7 +37,7 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.23.0
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
