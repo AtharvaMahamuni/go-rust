1d4996f707d9ee89c45d0fd4f924d2f7b5d31af4
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2024-08-27 01:02:14 +0530

    Interview questions

---
+++ Interview/SOLID/solid.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+/*
+S => Single Responsibility
+O => Open Closed Principle (Ready to take on new functionalities, without changing existing functionalities)
+L => Liskov Substitution Principle
+I => Interface Segregation Principle
+D => Dependency Inversion Principle
+
+*/
+
+func main() {
+	fmt.Println("Hello")
+}
